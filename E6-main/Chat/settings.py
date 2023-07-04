5844f48cb8478d998a94b38f39a175b28bb85d02
Jane <Jannyroux@yandex.ru>
2023-07-04 13:55:01 +0300

    EvgeniyaKruglova

--- E6-main/Chat/settings.py
+++ E6-main/Chat/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'sign',
     'protect',
 ]
-
+SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
